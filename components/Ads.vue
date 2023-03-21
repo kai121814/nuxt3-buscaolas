d98311e63f06b342303d885b0d418794f726bf0d
kai121814 <kaita.ostia@gmail.com>
2023-03-21 21:34:18 +0800

    first commit

---
+++ components/Ads.vue
@@ -0,0 +1,59 @@
+<script setup>
+</script>
+
+<template>
+  <div class="mt-6">
+    <div class="lg:w-9/12 mx-auto lg:flex go-premium-banner p-10" v-if="premium">
+      <div class="heading mb-8 lg:mb-0 font-bold uppercase text-center lg:text-left">
+        Con premium puedes ver hasta 7 dias de pronostico.
+      </div>
+      <div class="mx-auto">
+        <NuxtLink to="/premium">
+          <button class="go-premium text-white uppercase rounded-sm font-bold block mx-auto lg:mt-8 mb-5">
+            hazte premium
+          </button>
+        </NuxtLink>
+        <div class="text text-center">
+          Prueba 14 dias gratis y disfruta de los multiples beneficios de premium
+        </div>
+      </div>
+    </div>
+    <div class="lg:w-9/12 mx-auto lg:flex go-premium-banner" v-if="!premium">
+      <a :href="ads_info.url_basic" v-on:click="track(ads_info.id, ads_info.clicks)">
+        <img :src="ads_info.ads_url" alt="" class="">
+      </a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+ .go-premium-banner {
+    background: #F0F0F0;
+  }
+  .heading {
+    color: #1C496B;
+    width: 440px;
+    height: 120px;
+    font-size: 25px;
+  }
+  .go-premium {
+    background: #FFA800;
+    width: 200px;
+    height: 40px;
+  }
+  .text {
+    color: #3C3C3B;
+    width: 360px;
+    height: 40px;
+  }
+  @media only screen and (max-width: 1023px) {
+    .heading {
+      width: 100%;
+      height: auto;
+    }
+    .text {
+      width: 100%;
+      height: auto;
+    }
+  }
+  </style>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<script setup>
+useHead({
+  title: "Camaras de surf en vivo - Buscaolas",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: "Camaras de surf en las principales playas de Chile",
+    },
+    {
+      hid: "og:title",
+      name: "og:title",
+      content: "Buscaolas - camaras en vivo en las principales playas",
+    },
+    {
+      hid: "og:description",
+      name: "og:description",
+      content: "Camaras de surf en las principales playas de Chile",
+    },
+    {
+      hid: "og:url",
+      name: "og:url",
+      content: "https://buscaolas.cl",
+    },
+  ],
+});
+const ArticuloData = await useFetch("/api/main/home");
+// const art = ArticuloData.articulos1.slice(0, 3);
+const alert = false;
+</script>
+
+
+<template>
+  <div class="overflow-x-hidden">
+    <client-only>
+      <Header />
+      <Detect />
+    </client-only>
+    <Footer />
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  padding-top: 30px;
+  height: 0;
+  overflow: hidden;
+}
+
+.video-container iframe,
+.video-container object,
+.video-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
